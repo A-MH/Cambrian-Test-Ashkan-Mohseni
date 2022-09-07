82dec57a54c3dcaeec18384a3b870e4668420cf2
Ashkan <ashkan_m_h@hotmail.co.uk>
2022-09-07 22:41:15 +0100

    python file updated

--- Python Files/NetworkManager.py
+++ Python Files/NetworkManager.py
@@ -6,20 +6,28 @@ host, port = "192.168.1.41", 25001
 def setup_connection():
     global sock
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((host, port))
+    try:
+        sock.connect((host, port))
+    except:
+        setup_connection()
 
 def get_file_location():
     global sock
     file_location = ''
-    file_location = sock.recv(1024).decode("UTF-8") #receiveing data in Byte from C#, and converting it to String
+    try:
+        file_location = sock.recv(1024).decode("UTF-8") #receiveing data in Byte from C#, and converting it to String
+    except:
+        print("error detected in connection. restablishing connection")
     while file_location == '': # if received string is empty, it could mean the connection is lost
         sock.close()
         setup_connection() # try restablishing connection
-        file_location = sock.recv(1024).decode("UTF-8") #receiveing data in Byte from C#, and converting it to String
+        try:
+            file_location = sock.recv(1024).decode("UTF-8") #receiveing data in Byte from C#, and converting it to String
+        except (SyntaxError, IndexError) as E:
+            print(E)
 
     print(f"received: {file_location}")
     file_content = open(file_location, "r").read()
-    print(file_content)
     send_cube_info(file_content)
 
 def send_cube_info(file_content):
